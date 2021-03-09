ea94d3c32d011f8a9ba3f39329f059f2d966b6d5
Yeshaswini18 <yeshaswinivenkatesh18@gmail.com>
2021-03-09 13:27:51 -0800

    changes

--- WorldWeather/WorldWeather/ViewController.swift
+++ WorldWeather/WorldWeather/ViewController.swift
@@ -44,9 +44,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITableViewDe
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = Bundle.main.loadNibNamed("TableViewCell", owner: self, options: nil)?.first as! TableViewCell
-//        cell.lblDay.text = DailyForecastArr[indexPath.row].dayOfWeek
-//        cell.lblLow.text = String(DailyForecastArr[indexPath.row].minimumTemp) + "°F"
-//        cell.lblHigh.text = String(DailyForecastArr[indexPath.row].maximumTemp) + "°F"
         cell.setData(forecast: DailyForecastArr[indexPath.row])
         
         return cell
